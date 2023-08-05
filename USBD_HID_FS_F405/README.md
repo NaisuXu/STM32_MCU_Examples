e130e7f70a72dcee1c68b82901b1b90838af1aa2
naisu <naisuxu@qq.com>
2023-08-05 16:02:44 +0800

    add: USBD_HID_HS_F405 & USBD_HID_FS_F405

---
+++ USBD_HID_FS_F405/README.md
@@ -0,0 +1,94 @@
+# USBD_HID_FS_F405
+
+该示例演示USB作为HID设备使用，该示例实现和双向数据透传功能，一包可以发送64字节，设备在接收到来自主机的消息后进行回传。
+
+引脚使用为 `PA12 - DP` `PA11 - DM` ，`VID = 1155 (0x0483)` `PID = 22352 (0x5750)` 。
+
+除默认生成的代码，需要修改下面几处：
+
+**usbd_custom_hid_if.c**
+
+```c
+/** Usb HID report descriptor. */
+__ALIGN_BEGIN static uint8_t CUSTOM_HID_ReportDesc_FS[USBD_CUSTOM_HID_REPORT_DESC_SIZE] __ALIGN_END =
+{
+0x06, 0x00, 0xFF,  // Usage Page (Vendor Defined 0xFF00)
+0x09, 0x00,        // Usage (0x00)
+0xA1, 0x01,        // Collection (Application)
+0x09, 0x01,        //   Usage (0x01)
+0x15, 0x00,        //   Logical Minimum (0)
+0x26, 0xFF, 0x00,  //   Logical Maximum (255)
+0x75, 0x08,        //   Report Size (8)
+0x95, 0x40,        //   Report Count (64)
+0x81, 0x00,        //   Input (Data,Array,Abs,No Wrap,Linear,Preferred State,No Null Position)
+0x09, 0x02,        //   Usage (0x02)
+0x15, 0x00,        //   Logical Minimum (0)
+0x26, 0xFF, 0x00,  //   Logical Maximum (255)
+0x75, 0x08,        //   Report Size (8)
+0x95, 0x40,        //   Report Count (64)
+0x91, 0x00,        //   Output (Data,Array,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
+0xC0,              // End Collection
+// 34 bytes
+};
+```
+
+
+```c
+uint32_t size = 0;
+uint8_t buff[64];
+
+// 收到来自主机的数据时会触发该事件
+static int8_t CUSTOM_HID_OutEvent_FS(uint8_t event_idx, uint8_t state)
+{
+  UNUSED(event_idx);
+  UNUSED(state);
+
+  size = USBD_LL_GetRxDataSize(&hUsbDeviceFS, CUSTOM_HID_EPOUT_ADDR); // 获取收到的数据长度
+
+  USBD_CUSTOM_HID_HandleTypeDef *hhid = (USBD_CUSTOM_HID_HandleTypeDef *)(hUsbDeviceFS.pClassData);
+  for(int i=0; i<size; i++)
+  {
+	  buff[i]=hhid->Report_buf[i]; // 读取接收到的数据
+  }
+  
+  USBD_CUSTOM_HID_SendReport(&hUsbDeviceFS, buff, size);
+  
+  // 开启下一次接收
+  if (USBD_CUSTOM_HID_ReceivePacket(&hUsbDeviceFS) != (uint8_t)USBD_OK)
+  {
+	  return -1;
+  }
+
+  return (USBD_OK);
+}
+```
+
+
+
+**usbd_conf.h**
+
+```c
+/*---------- 接收缓冲区大小 -----------*/
+/*---------- 对于全速设备收和发一个包最大都为64字节 -----------*/
+#define USBD_CUSTOMHID_OUTREPORT_BUF_SIZE     64U
+/*---------- 报告描述符长度 -----------*/
+#define USBD_CUSTOM_HID_REPORT_DESC_SIZE     34U
+/*---------- 端电查询时间间隔 -----------*/
+/*---------- 对于全速设备该值为1表示最快可以每1ms通讯一次 -----------*/
+#define CUSTOM_HID_FS_BINTERVAL     0x5U
+/*---------- -----------*/
+```
+
+
+
+**usbd_customhid.h**
+
+```c
+// 接收一个包最大为64字节
+#define CUSTOM_HID_EPIN_SIZE        0x40U
+// 发送一个包最大为64字节
+#define CUSTOM_HID_EPOUT_SIZE        0x40U
+```
+
+
+
---
+++ USBD_HID_HS_F405/USB_DEVICE/App/usbd_custom_hid_if.c
@@ -0,0 +1,241 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : usbd_custom_hid_if.c
+  * @version        : v1.0_Cube
+  * @brief          : USB Device Custom HID interface file.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "usbd_custom_hid_if.h"
+
+/* USER CODE BEGIN INCLUDE */
+
+/* USER CODE END INCLUDE */
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE END PV */
+
+/** @addtogroup STM32_USB_OTG_DEVICE_LIBRARY
+  * @brief Usb device.
+  * @{
+  */
+
+/** @addtogroup USBD_CUSTOM_HID
+  * @{
+  */
+
+/** @defgroup USBD_CUSTOM_HID_Private_TypesDefinitions USBD_CUSTOM_HID_Private_TypesDefinitions
+  * @brief Private types.
+  * @{
+  */
+
+/* USER CODE BEGIN PRIVATE_TYPES */
+
+/* USER CODE END PRIVATE_TYPES */
+
+/**
+  * @}
+  */
+
+/** @defgroup USBD_CUSTOM_HID_Private_Defines USBD_CUSTOM_HID_Private_Defines
+  * @brief Private defines.
+  * @{
+  */
+
+/* USER CODE BEGIN PRIVATE_DEFINES */
+
+/* USER CODE END PRIVATE_DEFINES */
+
+/**
+  * @}
+  */
+
+/** @defgroup USBD_CUSTOM_HID_Private_Macros USBD_CUSTOM_HID_Private_Macros
+  * @brief Private macros.
+  * @{
+  */
+
+/* USER CODE BEGIN PRIVATE_MACRO */
+
+/* USER CODE END PRIVATE_MACRO */
+
+/**
+  * @}
+  */
+
+/** @defgroup USBD_CUSTOM_HID_Private_Variables USBD_CUSTOM_HID_Private_Variables
+  * @brief Private variables.
+  * @{
+  */
+
+/** Usb HID report descriptor. */
+__ALIGN_BEGIN static uint8_t CUSTOM_HID_ReportDesc_HS[USBD_CUSTOM_HID_REPORT_DESC_SIZE] __ALIGN_END =
+{
+0x06, 0x00, 0xFF,  // Usage Page (Vendor Defined 0xFF00)
+0x09, 0x00,        // Usage (0x00)
+0xA1, 0x01,        // Collection (Application)
+0x09, 0x01,        //   Usage (0x01)
+0x15, 0x00,        //   Logical Minimum (0)
+0x26, 0xFF, 0x00,  //   Logical Maximum (255)
+0x75, 0x08,        //   Report Size (8)
+0x96, 0x00, 0x04,  //   Report Count (1024)
+0x81, 0x00,        //   Input (Data,Array,Abs,No Wrap,Linear,Preferred State,No Null Position)
+0x09, 0x02,        //   Usage (0x02)
+0x15, 0x00,        //   Logical Minimum (0)
+0x26, 0xFF, 0x00,  //   Logical Maximum (255)
+0x75, 0x08,        //   Report Size (8)
+0x96, 0x00, 0x04,  //   Report Count (1024)
+0x91, 0x00,        //   Output (Data,Array,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
+0xC0,              // End Collection
+// 36 bytes
+};
+
+/* USER CODE BEGIN PRIVATE_VARIABLES */
+
+/* USER CODE END PRIVATE_VARIABLES */
+
+/**
+  * @}
+  */
+
+/** @defgroup USBD_CUSTOM_HID_Exported_Variables USBD_CUSTOM_HID_Exported_Variables
+  * @brief Public variables.
+  * @{
+  */
+
+extern USBD_HandleTypeDef hUsbDeviceHS;
+
+/* USER CODE BEGIN EXPORTED_VARIABLES */
+
+/* USER CODE END EXPORTED_VARIABLES */
+/**
+  * @}
+  */
+
+/** @defgroup USBD_CUSTOM_HID_Private_FunctionPrototypes USBD_CUSTOM_HID_Private_FunctionPrototypes
+  * @brief Private functions declaration.
+  * @{
+  */
+
+static int8_t CUSTOM_HID_Init_HS(void);
+static int8_t CUSTOM_HID_DeInit_HS(void);
+static int8_t CUSTOM_HID_OutEvent_HS(uint8_t event_idx, uint8_t state);
+
+/**
+  * @}
+  */
+
+USBD_CUSTOM_HID_ItfTypeDef USBD_CustomHID_fops_HS =
+{
+  CUSTOM_HID_ReportDesc_HS,
+  CUSTOM_HID_Init_HS,
+  CUSTOM_HID_DeInit_HS,
+  CUSTOM_HID_OutEvent_HS
+};
+
+/** @defgroup USBD_CUSTOM_HID_Private_Functions USBD_CUSTOM_HID_Private_Functions
+  * @brief Private functions.
+  * @{
+  */
+
+/* Private functions ---------------------------------------------------------*/
+
+/**
+  * @brief  Initializes the CUSTOM HID media low layer
+  * @retval USBD_OK if all operations are OK else USBD_FAIL
+  */
+static int8_t CUSTOM_HID_Init_HS(void)
+{
+  /* USER CODE BEGIN 8 */
+  return (USBD_OK);
+  /* USER CODE END 8 */
+}
+
+/**
+  * @brief  DeInitializes the CUSTOM HID media low layer
+  * @retval USBD_OK if all operations are OK else USBD_FAIL
+  */
+static int8_t CUSTOM_HID_DeInit_HS(void)
+{
+  /* USER CODE BEGIN 9 */
+  return (USBD_OK);
+  /* USER CODE END 9 */
+}
+
+uint32_t size = 0;
+uint8_t buff[1024];
+
+// 收到来自主机的数据时会触发该事件
+static int8_t CUSTOM_HID_OutEvent_HS(uint8_t event_idx, uint8_t state)
+{
+  UNUSED(event_idx);
+  UNUSED(state);
+
+  size = USBD_LL_GetRxDataSize(&hUsbDeviceHS, CUSTOM_HID_EPOUT_ADDR); // 获取收到的数据长度
+
+  USBD_CUSTOM_HID_HandleTypeDef *hhid = (USBD_CUSTOM_HID_HandleTypeDef *)(hUsbDeviceHS.pClassData);
+  for(int i=0; i<size; i++)
+  {
+	  buff[i]=hhid->Report_buf[i]; // 读取接收到的数据
+  }
+
+  USBD_CUSTOM_HID_SendReport(&hUsbDeviceHS, buff, size);
+
+  // 开启下一次接收
+  if (USBD_CUSTOM_HID_ReceivePacket(&hUsbDeviceHS) != (uint8_t)USBD_OK)
+  {
+	  return -1;
+  }
+
+  return (USBD_OK);
+}
+
+/* USER CODE BEGIN 11 */
+/**
+  * @brief  Send the report to the Host
+  * @param  report: The report to be sent
+  * @param  len: The report length
+  * @retval USBD_OK if all operations are OK else USBD_FAIL
+  */
+/*
+static int8_t USBD_CUSTOM_HID_SendReport_HS(uint8_t *report, uint16_t len)
+{
+  return USBD_CUSTOM_HID_SendReport(&hUsbDeviceHS, report, len);
+}
+*/
+/* USER CODE END 11 */
+
+/* USER CODE BEGIN PRIVATE_FUNCTIONS_IMPLEMENTATION */
+
+/* USER CODE END PRIVATE_FUNCTIONS_IMPLEMENTATION */
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
